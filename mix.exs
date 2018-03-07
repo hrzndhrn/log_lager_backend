bbb68065d473b411e76a5237e31c14f2fcf3c286
Marcus Kruse <esurk.sucram@gmail.com>
2018-03-07 12:26:23 +0100

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogLagerBackend.MixProject do
   def project do
     [
       app: :log_lager_backend,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.6",
       description: description(),
       package: package(),
